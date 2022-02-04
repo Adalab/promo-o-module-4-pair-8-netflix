70c7dd39a39de9e564e6a2d4eea9167002265649
AngelesMB <angeles.martinezbarrio@gmail.com>
2022-02-04 11:10:59 +0100

    Add view engine and page not found

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
-const express = require('express');
-const cors = require('cors');
-const moviesData = require('./data/movies.json');
-const users = require('./data/users.json');
+const express = require("express");
+const cors = require("cors");
+const moviesData = require("./data/movies.json");
+const users = require("./data/users.json");
 
 // create and config server
 const server = express();
 server.use(cors());
 server.use(express.json());
+server.set("view engine", "ejs");
 
 // init express aplication
 const serverPort = 4000;
@@ -14,19 +15,13 @@ server.listen(serverPort, () => {
   console.log(`Server listening at http://localhost:${serverPort}`);
 });
 
-const staticServerPathWeb = './src/public-react'; // En esta carpeta ponemos los ficheros estáticos
-server.use(express.static(staticServerPathWeb));
-
-const staticServerPathImages = './src/public-movies-images'; // En esta carpeta ponemos los ficheros estáticos
-server.use(express.static(staticServerPathImages));
-
 // api endpoints
-server.get('/movies', (req, res) => {
+server.get("/movies", (req, res) => {
   const genreFilterParam = req.query.genre;
   const sortFilterParam = req.query.sort;
   const filteredMovies = moviesData
     .filter((eachMovie) => {
-      if (genreFilterParam === '') {
+      if (genreFilterParam === "") {
         return eachMovie;
       } else {
         return eachMovie.gender === genreFilterParam;
@@ -34,7 +29,7 @@ server.get('/movies', (req, res) => {
     })
     .sort(function (a, b) {
       console.log(sortFilterParam);
-      if (sortFilterParam === 'asc') {
+      if (sortFilterParam === "asc") {
         if (a.title > b.title) {
           return 1;
         }
@@ -56,7 +51,7 @@ server.get('/movies', (req, res) => {
   });
 });
 
-server.post('/login', (req, res) => {
+server.post("/login", (req, res) => {
   console.log(req.body);
   const emailLogin = req.body.email;
   const passwordLogin = req.body.password;
@@ -72,7 +67,32 @@ server.post('/login', (req, res) => {
   } else {
     res.json({
       success: false,
-      errorMessage: 'Usuaria/o no encontrada/o',
+      errorMessage: "Usuaria/o no encontrada/o",
     });
   }
+});
+
+server.get("/movie/:movieId", (req, res) => {
+  const paramsId = req.params.movieId;
+  const foundMovie = moviesData.find((eachMovie) => eachMovie.id === paramsId);
+  if (foundMovie === undefined) {
+    const routeData = { route: req.url };
+    res.render("page-not-found", routeData);
+  } else {
+    res.render("movie", foundMovie);
+  }
+});
+
+const staticServerPathWeb = "./src/public-react"; // En esta carpeta ponemos los ficheros estáticos
+server.use(express.static(staticServerPathWeb));
+
+const staticServerPathImages = "./src/public-movies-images"; // En esta carpeta ponemos los ficheros estáticos
+server.use(express.static(staticServerPathImages));
+
+const staticServerPathMovieStyles = "./src/public-movie-styles"; // En esta carpeta ponemos los ficheros estáticos
+server.use(express.static(staticServerPathMovieStyles));
+
+server.get("*", (req, res) => {
+  const routeData = { route: req.url };
+  res.render("page-not-found", routeData);
 });
