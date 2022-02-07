839bcfdc2913a48120a4835bd5a9183e59f58860
AngelesMB <angeles.martinezbarrio@gmail.com>
2022-02-07 14:36:10 +0100

    Add styles to movie.ejs and refactor movies select

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 //const moviesData = require('./data/movies.json');
-const users = require('./data/users.json');
-const Database = require('better-sqlite3');
+const users = require("./data/users.json");
+const Database = require("better-sqlite3");
 
 // create and config server
 const server = express();
 server.use(cors());
 server.use(express.json());
-server.set('view engine', 'ejs');
+server.set("view engine", "ejs");
 
 // init express aplication
 const serverPort = 4000;
@@ -16,75 +16,55 @@ server.listen(serverPort, () => {
   console.log(`Server listening at http://localhost:${serverPort}`);
 });
 
-const db = new Database('./src/db/database.db', { verbose: console.log });
+const db = new Database("./src/db/database.db", { verbose: console.log });
 
 // api endpoints
-server.get('/movies', (req, res) => {
+server.get("/movies", (req, res) => {
   const genreFilterParam = req.query.genre;
   const sortFilterParam = req.query.sort;
-  if (genreFilterParam === '' && sortFilterParam === 'asc') {
-    const query = db.prepare('SELECT*FROM movies ORDER by name ASC');
-    const moviesData = query.all();
-    res.json({
-      success: true,
-      movies: moviesData,
-    });
-  } else if (genreFilterParam === '' && sortFilterParam === 'desc') {
-    const query = db.prepare('SELECT*FROM movies ORDER by name DESC');
-    const moviesData = query.all();
-    res.json({
-      success: true,
-      movies: moviesData,
-    });
-  } else if (sortFilterParam === 'asc') {
-    const queryGenre = db.prepare(
-      'SELECT*FROM movies WHERE genre =? ORDER by name ASC'
-    );
-    const filterData = queryGenre.all(genreFilterParam);
-    res.json({
-      success: true,
-      movies: filterData,
-    });
-  } else {
-    const queryGenre = db.prepare(
-      'SELECT*FROM movies WHERE genre =? ORDER by name DESC'
-    );
-    const filterData = queryGenre.all(genreFilterParam);
-    res.json({
-      success: true,
-      movies: filterData,
-    });
-  }
-
-  // const filteredMovies = moviesData
-  // .filter((eachMovie) => {
-  //   if (genreFilterParam === '') {
-  //     return eachMovie;
-  //   } else {
-  //     return eachMovie.gender === genreFilterParam;
-  //   }
-  // })
-  // .sort(function (a, b) {
-  //   console.log(sortFilterParam);
-  //   if (sortFilterParam === 'asc') {
-  //     if (a.title > b.title) {
-  //       return 1;
-  //     }
-  //     if (a.title < b.title) {
-  //       return -1;
-  //     }
-  //   } else {
-  //     if (a.title > b.title) {
-  //       return -1;
-  //     }
-  //     if (a.title < b.title) {
-  //       return 1;
-  //     }
-  //   }
+  // const sortFilterParam = req.query.sort.toUpperCase();
+  console.log(sortFilterParam);
+  let moviesData = [];
+  // if (genreFilterParam === "") {
+  //   const query = db.prepare("SELECT*FROM movies ORDER BY name ?");
+  //   moviesData = query.all(sortFilterParam);
+  // } else {
+  //   const queryGenre = db.prepare(
+  //     "SELECT*FROM movies WHERE genre =? ORDER BY name ?"
+  //   );
+  //   moviesData = queryGenre.all(genreFilterParam, sortFilterParam);
+  // }
+  // res.json({
+  //   success: true,
+  //   movies: moviesData,
   // });
+
+  if (genreFilterParam === "") {
+    let query;
+    if (sortFilterParam === "asc") {
+      query = db.prepare("SELECT*FROM movies ORDER BY name");
+    } else {
+      query = db.prepare("SELECT*FROM movies ORDER BY name DESC");
+    }
+    moviesData = query.all();
+  } else {
+    let query;
+    if (sortFilterParam === "asc") {
+      query = db.prepare("SELECT*FROM movies WHERE genre =? ORDER BY name");
+    } else {
+      query = db.prepare(
+        "SELECT*FROM movies WHERE genre =? ORDER BY name DESC"
+      );
+    }
+    moviesData = query.all(genreFilterParam);
+  }
+  res.json({
+    success: true,
+    movies: moviesData,
+  });
 });
 
-server.post('/login', (req, res) => {
+server.post("/login", (req, res) => {
   console.log(req.body);
   const emailLogin = req.body.email;
   const passwordLogin = req.body.password;
@@ -100,34 +80,34 @@ server.post('/login', (req, res) => {
   } else {
     res.json({
       success: false,
-      errorMessage: 'Usuaria/o no encontrada/o',
+      errorMessage: "Usuaria/o no encontrada/o",
     });
   }
 });
 
-server.get('/movie/:movieId', (req, res) => {
+server.get("/movie/:movieId", (req, res) => {
   const paramsId = req.params.movieId;
-  const query = db.prepare('SELECT * FROM movies WHERE id =?');
+  const query = db.prepare("SELECT * FROM movies WHERE id =?");
   const foundMovie = query.get(paramsId);
   // const foundMovie = moviesData.find((eachMovie) => eachMovie.id === paramsId);
   if (foundMovie === undefined) {
     const routeData = { route: req.url };
-    res.render('page-not-found', routeData);
+    res.render("page-not-found", routeData);
   } else {
-    res.render('movie', foundMovie);
+    res.render("movie", foundMovie);
   }
 });
 
-const staticServerPathWeb = './src/public-react'; // En esta carpeta ponemos los ficheros estáticos
+const staticServerPathWeb = "./src/public-react"; // En esta carpeta ponemos los ficheros estáticos
 server.use(express.static(staticServerPathWeb));
 
-const staticServerPathImages = './src/public-movies-images'; // En esta carpeta ponemos los ficheros estáticos
+const staticServerPathImages = "./src/public-movies-images"; // En esta carpeta ponemos los ficheros estáticos
 server.use(express.static(staticServerPathImages));
 
-const staticServerPathMovieStyles = './src/public-movie-styles'; // En esta carpeta ponemos los ficheros estáticos
+const staticServerPathMovieStyles = "./src/public-movie-styles"; // En esta carpeta ponemos los ficheros estáticos
 server.use(express.static(staticServerPathMovieStyles));
 
-server.get('*', (req, res) => {
+server.get("*", (req, res) => {
   const routeData = { route: req.url };
-  res.render('page-not-found', routeData);
+  res.render("page-not-found", routeData);
 });
